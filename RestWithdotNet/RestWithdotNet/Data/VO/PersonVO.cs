a5a31ea3e3a227a3c0e89ff26dab212cf7065083
Henrique Soares <62728244+henriquesph@users.noreply.github.com>
2024-01-23 16:59:20 -0300

    serialization

--- RestWithdotNet/RestWithdotNet/Data/VO/PersonVO.cs
+++ RestWithdotNet/RestWithdotNet/Data/VO/PersonVO.cs
@@ -1,11 +1,19 @@
-﻿namespace RestWithDotNet.Data.VO
+﻿/*using System.Text.Json.Serialization;*/ // custom serialization
+
+namespace RestWithDotNet.Data.VO
 {
     public class PersonVO
     {
+        /*[JsonPropertyName("code")] */// mudando a serialização no Json - personalizando o nome do campo exibido
+        // servve tanto para ler os dados quanto para persistir no BD
         public long Id { get; set; }
+        //[JsonPropertyName("name")]
         public string FirstName { get; set; }
+        //[JsonPropertyName("last_name")]
         public string LastName { get; set; }
+        //[JsonIgnore] // não vai ser serializado - não aparece no Json
         public string Adress { get; set; }
+        //[JsonPropertyName("sex")]
         public string Gender { get; set; }
     }
 }
